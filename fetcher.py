8cd6eb9349c3783b75834de589bacfa2f71e9321
iqfareez <iqmal3@outlook.com>
2021-10-01 20:34:49 +0800

    :zap: Done
    i guess...

--- fetcher.py
+++ fetcher.py
@@ -1,37 +1,63 @@
 import json
+import os
 import time
 from datetime import datetime
-from zoneinfo import ZoneInfo
 
 import requests
 
-reqUrl = "http://api.open-notify.org/astros.json"
+AZURE_KEY1 = os.environ['AZURE_KEY1']
+# AZURE_KEY2 = os.environ['AZURE_KEY2']
 
 data = {}
+people = []
 
 print('\nStarting\n')
 
-attempt_count = 0
+reqUrl = "http://api.open-notify.org/astros.json"
+response = requests.get(reqUrl)
+json_response = response.json()
 
-# Retry if data still empty
-while not data:
-    if (attempt_count > 0):
-        print(f'\nRetrying failed requests.\n')
+# Only put into json if everything's fine
+if (response.status_code == 200) and json_response['message'] == 'success':
+    print(f"Got data")
+    number = json_response['number']  # original response
+    message = json_response['message']  # original response
 
-    response = requests.get(reqUrl)
-    json_response = response.json()
+    for item in json_response['people']:
+        name = item['name']  # original response
+        craft = item['craft']  # original response
+        print('Adding {}'.format(name))
+        bingReq = requests.get(
+            'https://api.bing.microsoft.com/v7.0/images/search',
+            headers={'Ocp-Apim-Subscription-Key': AZURE_KEY1},
+            params={
+                'q': name,
+                'count': 1
+            })
+        if bingReq.status_code == 200:
+            bingResponse = bingReq.json()
+            people.append({
+                'name':
+                name,
+                'craft':
+                craft,
+                'thumbnailUrl':
+                bingResponse['value'][0]['thumbnailUrl'],
+                'contentUrl':
+                bingResponse['value'][0]['contentUrl']
+            })
 
-    # Only put into json if everything's fine
-    if (response.status_code == 200) and json_response['message'] == 'success':
-        print(f"Got data")
-        data['astros'] = json_response
-    else:
-        print(f'Failed ({response.status_code})')
+        time.sleep(1)  # delay a bit
 
-    attempt_count += 1
+    # rebuild response
+    data['data'] = {'people': people, 'number': number, 'message': message}
 
-fetch_finish_utc = time.strftime("%a, %d %b %Y %H:%M:%S %Z")  # format UTC Time
-print(f'Fetching finish at {fetch_finish_utc}')
+else:
+    print(f'Failed ({response.status_code})')
+    raise SystemExit()
+
+fetch_finish = time.strftime("%a, %d %b %Y %H:%M:%S %Z")  # format UTC Time
+print(f'Fetching finish at {fetch_finish}')
 
 # writing all location data to file
 with open('db.json', 'w') as outfile:
@@ -39,7 +65,7 @@ with open('db.json', 'w') as outfile:
     print('\nFinish writing to db.json')
 
 log = {}
-log['fetcher_last_run'] = fetch_finish_utc
+log['fetcher_last_run'] = fetch_finish
 
 # writing log
 with open('public/log.json', 'w') as outfile:
